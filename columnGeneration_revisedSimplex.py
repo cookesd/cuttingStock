9b8ae31c8fecbb48eacd7e127ca58198af368a7c
cookesd <51008829+cookesd@users.noreply.github.com>
2019-07-07 18:21:04 -0500

    Add files via upload

--- columnGeneration_revisedSimplex.py
+++ columnGeneration_revisedSimplex.py
@@ -12,17 +12,20 @@ from collections.abc import Iterable
 import os
 
 ##### knapsack solution #####
-def knapsack(values, weights, capacity,solVect):
+def knapsack(values, weights, capacity,solVect = None):
     '''Solves the unbounded knapsack problem using dynamic programming (recursion).
     The unbounded knapsack problem here tries to maximize the value (dual variable of the entering cut pattern)
     subject to the capacity constraints (the board cuts cannot exceed the board length).
     This new pattern will enter our basis if the value (dual var) is greater than 1,
     Otherwise, it will not improve the objective to the linear program.
     
-    @param values (iterable of floats) the current dual variables for the linear programming solution (c_{B}B^{-1})
-    @param weights (iterable of floats) the length of the desired cuts
-    @param capacity (float) the knapsack capacity (length of the board)
-    @param solVect (iterable of length number of cuts) can be a list of zeros initially; used for recursively calling knapsack
+    @param values (iterable of floats) : the current dual variables for the linear programming solution (c_{B}B^{-1})
+    @param weights (iterable of floats) : the length of the desired cuts
+    @param capacity (float) : the knapsack capacity (length of the board)
+    @param solVect (iterable of length number of cuts) : can be a list of zeros initially;
+    used for recursively calling knapsack; if no value specified, then automatically sets to list of zeros
+    
+    returns finalSol : the solution to the knapsack ()
     '''
 #    if verbose:
 #        print(solVect)
@@ -57,7 +60,12 @@ def findLV(Binv,b,a,tol = -1.0E-12):
     @param Binv, the inverse matrix of the current basis
     @param b, the original right hand side of the constraints
     @param a, the column vector of the entering variable
-    returns the row of the leaving variable (also column in Binv) or identifies unboundedness'''
+    
+    returns unbounded : boolean True if unboundedness detected; false o/w
+    returns lvrow[0][0] : the row of the leaving variable from ratio test (lowest index chosen in case of ties)
+    returns bbar : the costs of the basic variables
+    returns abar : the column of the entering variable in the current basic feasible solution (B^{-1}*a_i for entering varable i)
+    '''
     largeNum = max(b)**2 #this should be big enough, need to find better value
     bbar = np.matmul(Binv,b)
     abar = np.matmul(Binv,a)
@@ -70,31 +78,51 @@ def findLV(Binv,b,a,tol = -1.0E-12):
         ratios = np.array(ratList) # gets elementwise quotient of the vectors
     lvrow = np.where(ratios==min(ratios)) #finds row of the minimum ratio (one that goes to zero fastest after pivot)
     minRatio = ratios[lvrow[0][0]] #the minimum ratio
-    print(lvrow)
-    print('ratios',ratios)
-    print('min ratio',minRatio)
+#    print('rows with min ratio',lvrow)
+#    print('ratios',ratios)
+#    print('min ratio',minRatio)
     unbounded = minRatio < tol #the problem is unbounded if this minimum ratio is negative
-    print('unbounded',unbounded)
     return(unbounded,lvrow[0][0],bbar,abar)
 
 def updateBinv(Binv,abar,lvrow):
+    '''
+    Updates the B^{-1} matrix with the new col (abar) in place of the leaving column (same column as lvrow since square matrix)
+    
+    @param Binv : the B^{-1} matrix from the previous solution
+    @param abar : the column for the entering basic variable when premultiplied by the current B^{-1} matrix
+    (B^{-1}*a_i for new basic variable i)
+    @para lvrow : the row corresponding to the leaving variable found using the ratio test (b_j / a_{ij}). In case of ties, lowest index chosen
+    
+    returns newBinv : the updated B^{-1} matrix
+    '''
     matDim = len(Binv)
     eMat = np.identity(matDim) #identity matrix with same size as Binv
-    newCol = -abar/abar[lvrow] #the lvrowth column should be -abar_ik/abar_rk with r,r element = 1/abar_rk
+    newCol = -abar/abar[lvrow] #the lvrowth column (r) should be -abar_ik/abar_rk with (r,r) element = 1/abar_rk
     newCol[lvrow] = 1/abar[lvrow]
-    print(newCol)
-    eMat[:,lvrow] = np.reshape(newCol,(1,matDim))
+    print('entering column\n', newCol)
+    eMat[:,lvrow] = np.reshape(newCol,(1,matDim)) # places newCol into the lvrow column
     
-    newBinv = np.matmul(eMat,Binv)
+    newBinv = np.matmul(eMat,Binv) #updates B^{-1} using E*B^{-1}
     return(newBinv)
     
 def calcDualVars(cB,Binv):
+    '''Uses matrix multiplication to calculate c_B*B{^-1} (dual variables w for the current solution)
+    
+    @param cB : the cost coefficients of the basic variables
+    @param Binv : the B^{-1} matrix for the current basis
+    
+    returns cBBinv : the current dual variables (w = c_B*B^{-1})
+    '''
     cBBinv = np.matmul(cB,Binv)
     return(cBBinv)
 
 
 #pre and post processing
 def printResult(resultDict):
+    '''Prints the result in a tabular format to the console
+    
+    @param resultDict the dictionary of cut patterns with quantities of each cut size
+    '''
     for pattern in resultDict:
         print('\n',resultDict[pattern]['patternQuantity'][0],' (',resultDict[pattern]['patternQuantity'][1],') Cuts of Pattern ',pattern[len(pattern)-1],':',sep='')
         for cut in resultDict[pattern]:
@@ -113,7 +141,48 @@ def buildModel(fName = None,bLength = None,lenDict = None):
         assert isinstance(fName,str), 'Filename must be a string'
         assert os.path.exists(fName), 'This is not a valid path'
     
-
+# plotting as bar graph
+def plotResult(cut_dict):
+    '''Plots the result as a stacked bar graph using matplotlib.pyplot interface for pd.DataFrame
+    
+    @param cut_dict the dictionary of cut patterns with quantities of each cut size
+    '''
+    cutDF = pd.DataFrame(cut_dict)
+    cutsPerPattern = cutDF.drop(['patternQuantity','waste'],axis=0).sum()
+    maxNumCuts = int(max(cutsPerPattern))
+    rows = ['cut'+str(i+1) for i in range(maxNumCuts)]
+    cols = [col for col in cutDF.columns]
+    rows.append('waste')
+    
+    plotDF = pd.DataFrame(index=rows,columns = cols) #makes empty df with appropriate row/cols & names
+    
+    for pattern in cutDF.columns:
+        cutNum=1
+        patternDict = {}
+        for index in cutDF.drop(['patternQuantity','waste']).index:
+            #accounts for duplicate cuts of the same size & skips sizes with zero cuts
+            if cutDF[pattern][index]>0:
+                for i in range(int(cutDF[pattern][index])):
+                    patternDict['cut'+str(cutNum)]=float(index)
+                    cutNum+=1
+        #fills remaining cuts with zero lengths
+        if cutNum <= maxNumCuts:
+            remainingCuts = maxNumCuts+1-cutNum
+            for i in range(int(remainingCuts)):
+                patternDict['cut'+str(cutNum+i)]=0
+        patternDict['waste']=cutDF[pattern]['waste'] #adds the waste row
+        patternSeries = pd.Series(patternDict) #makes dict a series
+        plotDF[pattern] = patternSeries #adds the series for the pattern in the appropriate col in cutDF
+    
+    plotDF = plotDF.transpose() #transposes plotDF to make the stacked bar plot
+    
+    #plots cut patterns using stacked bargraphs
+    ax =plotDF.plot(kind='bar',stacked=True,edgecolor='k') #plots stacked bar graph
+    for p in ax.patches:
+        #plots cut size in each bar if height > 0.0
+        if p.get_height()>0.0:
+            ax.annotate(str(p.get_height()), (p.get_x()+.25*p.get_width(),p.get_y()+.5*p.get_height()))
+    
     
         
         
@@ -164,12 +233,11 @@ while benefit-1>epsilon:
         enteringCol = np.reshape(enteringCol,(len(enteringCol),1)) #ensures this is column vector
     else:
         print('The problem is unbounded')
-    print('B',B)
-    print('bbar',bbar)
-    print('cb',cB)
-    print('lv',lv)
-    print('benefit',benefit)
-    input('press enter to continue')
+    print('B\n',B)
+    print('bbar\n',bbar)
+    print('cb\n',cB)
+    print('lv\n',lv)
+    print('benefit\n',benefit,'\n')
 
 unbounded,lv,bbar,abar=findLV(Binv,b,enteringCol)
 if not unbounded:
@@ -191,7 +259,7 @@ for colNum in range(len(bbar)):
     cutDict[dictKey]['waste']=waste
     
 printResult(cutDict)
-
+plotResult(cutDict)
 
 
 
