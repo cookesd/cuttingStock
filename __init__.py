14a84112591de34aeede401368ab132f96daf9c3
cookesd <51008829+cookesd@users.noreply.github.com>
2020-08-08 15:48:40 -0500

    Only import exact classes, not solving method

---
+++ __init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Aug 18 16:53:28 2019
+
+@author: dakar
+"""
+
+from .classes import CuttingStock, CuttingStockSolution
+from .cutting_stock import cuttingStockProblem
