45002f4f5eb4222c46ef1f67aab1691c3d418e96
cookesd <stuart.d.cooke@gmail.com>
2020-08-14 10:40:26 -0500

    Delete subdirectory because files present after git pull.

--- cuttingstock/README.md
+++
@@ -1,3 +0,0 @@
-# cuttingStock
-Solves cutting stock problem using column generation
-Solves the column generation by solving a unbounded knapsack problem using dynamic programming (recursion)
--- cuttingstock/classes/__init__.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sun Aug 18 16:53:28 2019
-
-@author: dakar
-"""
-
-from .CuttingStock import CuttingStock
-from .CuttingStockSolution import CuttingStockSolution
